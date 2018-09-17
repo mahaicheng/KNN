eba4a371e93d917cd697aa21f2d9ac61796a415a
mahaicheng <hchma@outlook.com>
2018-09-17 15:24:54 +0800

    add print
    print x,y,k

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     try:
         x,y,k = input('input x, y, k\n').split()
         #x,y,k=1,1,2
+		print(x, y, k)
         probLabel = KNN.classify([int(x),int(y)],group, labels, int(k))
         print(probLabel)
     except Exception as e:
