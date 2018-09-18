d0f575b32c4395c6f60a245b6b33a265fadd2ef1
mahaicheng <hchma@outlook.com>
2018-09-18 11:39:23 +0800

    delete unneeded codes

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ if __name__ == '__main__':
     group,labels = KNN.createDataSet()
     try:
         x,y,k = input('input x, y, k\n').split()
-        #x,y,k=1,1,2
-		print(x, y, k)
         probLabel = KNN.classify([int(x),int(y)],group, labels, int(k))
         print(probLabel)
     except Exception as e:
