ca0a050e2fe616fd1be94eaf74b945d40d49fbf9
mahaicheng <hchma@outlook.com>
2018-12-01 16:46:23 +0800

    format

--- KNN.py
+++ KNN.py
@@ -4,16 +4,18 @@
 from numpy import *
 import operator
 
+
 def createDataSet():
-    group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
     labels = ['A', 'A', 'B', 'B']
-    return group,labels
+    return group, labels
+
 
 def classify(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
     diffMat = tile(inX, (dataSetSize, 1)) - dataSet
     sqDiffMat = diffMat**2
-    sqDistance = sqDiffMat.sum(axis = 1)
+    sqDistance = sqDiffMat.sum(axis=1)
     distance = sqDistance**0.5
     sortedDistIndesticy = distance.argsort()
 
@@ -23,5 +25,6 @@ def classify(inX, dataSet, labels, k):
         voteLabels = labels[sortedDistIndesticy[i]]
         classCount[voteLabels] = classCount.get(voteLabels, 0) + 1
 
-    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    sortedClassCount = sorted(
+        classCount.items(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
--- main.py
+++ main.py
@@ -4,13 +4,12 @@
 import KNN
 
 if __name__ == '__main__':
-    group,labels = KNN.createDataSet()
+    group, labels = KNN.createDataSet()
     try:
-        x,y,k = input('input x, y, k\n').split()
-        #x,y,k=1,1,2
-		print(x, y, k)
-        probLabel = KNN.classify([int(x),int(y)],group, labels, int(k))
+        x, y, k = input('input x, y, k\n').split()
+        # x,y,k=1,1,2
+        print(x, y, k)
+        probLabel = KNN.classify([int(x), int(y)], group, labels, int(k))
         print(probLabel)
     except Exception as e:
         print(e)
-
